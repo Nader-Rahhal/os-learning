02611904444294f0889ca6fbbf2d807b8e4f3326
Nader Rahhal <107228500+Nader-Rahhal@users.noreply.github.com>
2024-11-28 16:59:17 -0600

    new stuff

--- process-running/moderator.c
+++ process-running/moderator.c
@@ -8,18 +8,46 @@
 #define BUFFER 1024
 #define MAX_ARGS 12
 
+char *greetings[] = {
+            "Salutations, Mahdi!?\n",
+            "Jolly day Mods! Hope yours is also jolly.\n",
+            "RoOoOoOo!!!!\n"
+};
+
+char *jokes[] = {
+    "The Al Aqsa to pervert pipeline has been allgedly growing in recent years I've heard...\n",
+    "Never forget the great tragedy of Ghareebathon 2024, where an innocent mango lasi was britually ran through nonconsentually through the mouths of thousands...\n",
+    "The Armenian 'Genocide'\n",
+    "The Armenian 'Genocide'\n", // one with the link to the generator
+    "Did you know in Hand if you are 45 degrees North of the Dealer and a nationality that borders the nationality of the player to your righthand side during March of a the leap year, you can discard an extra card?\n",
+    "Wait, stop omg. You're lowkey eating rn\n",
+    "An emotional and gentle man.\n",
+    "Me when the Andersonville Village Discount aware (THOSE WHO KNOW)!\n",
+    "Malik did not die for this...\n",
+    "Strongest Kabobi rotten tomato enjoyer when they meet the weakest Black Bean Burger enthusiast\n",
+    "Omg Modi!!! I loved that tabooley of urs smmmmmmmmmmmmmm!!\n",
+    "Hyped for the next JanFanCam x Moderationator collab\n",
+    "That god awful photo taken at Ghareeb. You know the one I am referring to.\n",
+    "Gooners genuinly do anything for the huzz...\n",
+    "You're basically a POC in comparison to Nader if I am being honest.\n",
+    "'Mahdi you're sooo big bro coded. Have I told you that before\n-- Aala'\n",
+    "I swear to god no one ever fucked me so brutal that they pinned me down.\n",
+    "Did you just call yourself falastini?..\n",
+    "Average pantaloons wearer.\n",
+    "One day Mahdi, you will find someone who will take care of you the way Nader takes cares of his socks.\n",
+    "It is on nights like these that you have to remember the RoOoOoOo is always there for you.\n",
+};
+
 int main(){
+
+    printf("%s", greetings[0]);
+    printf("I am a multipurpose shell - I can run commands, tell jokes, and much more!\n");
 
     while (1){
 
-        char line[100];
+        printf("> ");
 
-        char *greetings[] = {
-            "Hello, Mahdi! What shall we embark on today?\n",
-            "Oh Moderator! Is that you?\nI was... uh ... busy.\nKnock next time before you come in please!\nAnyways what shall we do today?\n",
-            "Jolly day! What is the move on this fine evening?\n",
-            ""
-        };
+        char line[100];
 
 
         if(!fgets(line, BUFFER, stdin)) break;
@@ -41,16 +69,27 @@ int main(){
 
         args[arg_count] = NULL;
 
-        int rc = fork();
-
-        if (rc == 0){
-            execvp(args[0], args);
-            exit(0);
+        if (strcmp(line,"menu") == 0){
+            printf("Custom Commands:\n");
+            printf("lol - runs joke simulator\n");
+            printf("Please contact rahhalnader04@gmail.com with any thoughts.\n");
         }
 
         else {
-            wait(NULL);
+
+            int rc = fork();
+
+            if (rc == 0){
+                execvp(args[0], args);
+                exit(0);
+            }
+
+            else {
+                wait(NULL);
+            }
         }
+
+
     
     }
     return 0;
